262d11ff969ead0c345321e9ea960adf30510c0b
Eren Ali Şavk <esavk22@posta.pau.edu.tr>
2024-06-04 09:11:26 +0300

    fix

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,16 +1,9 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { $Enums, User } from '@prisma/client';
-import { Exclude } from 'class-transformer';
 import { $Enums, User } from '@prisma/client';
 
 export class UserEntity implements User {
   constructor(partial: Partial<UserEntity>) {
     Object.assign(this, partial);
   }
-  name: string;
-  role: $Enums.Role;
-
-  @ApiProperty()
   id: string;
   email: string;
   name: string;
